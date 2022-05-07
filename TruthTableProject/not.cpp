51b1d181192be2c8e243bc77c8ac3f96205e4669
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-07 17:50:38 +0300

    Реализован метод Not::getValue
    Реализован метод возврата значения операции инверсия Not::getValue.

--- TruthTableProject/not.cpp
+++ TruthTableProject/not.cpp
@@ -10,5 +10,10 @@ Not::~Not() { }
 
 int Not::getValue()
 {
-    return 0;
+    Node* operand = this->getLeftOperand(); // операнд выражения
+
+    if (operand != NULL) // Если операнд определен
+        return !operand->getValue(); // вернуть результат инверсии значения операнда
+    else
+        return 0;
 }

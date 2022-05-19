099119a419f7714512ff453780cc6794faa2dcb7
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-19 19:18:14 +0300

    Реализован метод isRootNodeCorrect
    Добавлена реализация метода, проверяющего корректность корневого узла xml файла.

--- TruthTableProject/errorcontroller.cpp
+++ TruthTableProject/errorcontroller.cpp
@@ -4,6 +4,17 @@ ErrorController::ErrorController() { }
 
 bool ErrorController::isRootNodeCorrect(QDomElement xRootNode)
 {
+    QDomNodeList children = xRootNode.childNodes();
+
+    if (children.length() < 1) // если дочерние узлы отсутствуют
+        throwError(NO_CHILD_NODES_IN_ROOT);
+
+    else if (children.length() > 1) // если дочерних узлов слишком много
+        throwError(TOO_MANY_CHILD_NODES_IN_ROOT);
+
+    else if (children.item(0).toElement().tagName() == "variable") // если дочерний узел - переменная
+        throwError(VARIABLE_IN_ROOT);
+
     return true;
 }
 
@@ -19,5 +30,12 @@ bool ErrorController::isOperationCorrect(QDomNode &xNode)
 
 void ErrorController::throwError(ErrorType errorType, QString *additionMsg)
 {
-    return;
+    Error error;
+    error.errorType = errorType;
+    error.errorMessege = errorMessages[(int)errorType];
+
+    if (additionMsg != NULL)
+        error.errorMessege = error.errorMessege.arg(*additionMsg);
+
+    throw error;
 }

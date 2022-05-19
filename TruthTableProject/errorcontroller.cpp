8777a7d2baaac8253bf5a1d8c2af13a92635b92e
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-19 21:17:42 +0300

    Реализован метод isVariableCorrect
    Добавлена реализация метода, осуществляющего проверку узла переменной.

--- TruthTableProject/errorcontroller.cpp
+++ TruthTableProject/errorcontroller.cpp
@@ -21,7 +21,27 @@ bool ErrorController::isRootNodeCorrect(QDomElement xRootNode)
 
 bool ErrorController::isVariableCorrect(QDomNode xNode)
 {
-    return false;
+    QRegExp rx("^[a-zA-Z0-9_]+$");
+    QDomNodeList children = xNode.childNodes();
+    QDomNamedNodeMap attributes = xNode.attributes();
+    QString variableName = xNode.toElement().text().trimmed();
+
+    if (variableName.isEmpty())
+        throwError(INCORRECT_VARIABLE_NAME, &variableName);
+
+    else if (variableName.length() > 20)
+        throwError(TOO_LONG_VARIABLE_NAME, &variableName);
+
+    else if (!rx.exactMatch(variableName) || variableName[0].isNumber())
+        throwError(INCORRECT_VARIABLE_NAME, &variableName);
+
+    else if (children.length() > 1 || children.item(0).nodeType() != QDomNode::TextNode)
+        throwError(CHILDREN_IN_VARIABLE, &variableName);
+
+    else if (attributes.length() > 0)
+        throwError(ATTRIBUTE_IN_VARIABLE, &variableName);
+
+    return true;
 }
 
 bool ErrorController::isOperationCorrect(QDomNode &xNode)

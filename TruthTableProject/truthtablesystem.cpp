150b000c861eb30f26d93f3335c145bead9321f6
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-21 14:31:48 +0300

    Реализован метод parseExpression
    Добавлена реализация метода, преобразующего входное дерево и раскладывающего его в вектор.

--- TruthTableProject/truthtablesystem.cpp
+++ TruthTableProject/truthtablesystem.cpp
@@ -23,7 +23,47 @@ QStringList TruthTableSystem::getTruthTable(QVector<QVector<int>> &values)
 
 Node* TruthTableSystem::parseExpression(QDomNode &xNode)
 {
-    return NULL;
+    Node* result;
+    if (xNode.toElement().tagName() == "variable") // если xml узел соответствует узлу переменной
+    {
+        // Создать новый узел переменной, со строковым представлением равным имени переменной
+        errorController.isVariableCorrect(xNode);
+        Variable* variable = new Variable();
+        variable->setStringId(xNode.toElement().text().trimmed());
+        result = variable;
+    }
+    else
+    {
+        // Создать операцию типа, содержащегося в атрибуте type xml узла
+        errorController.isOperationCorrect(xNode);
+        Operation* operation = Operation::getOperation(xNode.toElement().attributeNode("type").value());
+
+        QDomNodeList children = xNode.childNodes();
+        Node* childNodes[2] = {NULL, NULL};
+        for (int i = 0; i < children.length(); i++) // для всех дочерних узлов xml узла
+        {
+            // Преобразовать дочерний узел xml узла в объект класса Node
+            QDomNode child = children.item(i);
+            childNodes[i] = parseExpression(child);
+        }
+
+        operation->setOperands(childNodes[0], childNodes[1]); // задать дочернии узлы операндами данной операции
+        result = operation;
+    }
+
+    Node* desiredNode = findNode(result); // найти созданный узел в векторе
+    if (desiredNode != NULL) // если узел найден
+    {
+        // Вернуть указатель на найденный узел
+        delete result;
+        return desiredNode;
+    }
+    else
+    {
+        // Добавить созданный узел в вектор
+        variablesAmount += insertNode(result); // если была добавлена переменная, инкрементировать кол-во переменных
+        return result; // вернуть указатель на созданный узел
+    }
 }
 
 Node* TruthTableSystem::findNode(Node *node)
